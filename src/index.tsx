da5fbcab99206e827c1a1a06c741cbc19b336a5e
M-Husein <m.husein27@gmail.com>
2024-05-22 17:26:59 +0700

    feat: add custom theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, createTheme } from '@mantine/core';
 import App from "./App";
 
 import '@mantine/core/styles.css';
 import './App.css';
 
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
 createRoot(document.getElementById("app") as HTMLElement).render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider
+      theme={theme}
+    >
       <App />
     </MantineProvider>
   </React.StrictMode>
